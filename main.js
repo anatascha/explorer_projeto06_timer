2df46652c1cdf2f4b9fc75fc6b00826943c47c01
Natascha <117412528+anatascha@users.noreply.github.com>
2024-06-27 12:36:32 -0400

    card adjustment - main.js

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ function stopAllSounds() {
   });
 }
 
-function handleCardClick(audioId, imgId, hoverId) {
+function handleCardClick(audioId, imgId, hoverId, NhoverId1, NhoverId2, NhoverId3, Nimg1, Nimg2, Nimg3) {
   const audioElement = document.getElementById(audioId);
   if (!audioElement.paused) {
     audioElement.pause();
@@ -88,23 +88,30 @@ function handleCardClick(audioId, imgId, hoverId) {
     audioElement.play();
     document.getElementById(imgId).classList.add("hidden");
     document.getElementById(hoverId).classList.remove("hidden");
+    document.getElementById(NhoverId1).classList.add("hidden");
+    document.getElementById(NhoverId2).classList.add("hidden");
+    document.getElementById(NhoverId3).classList.add("hidden");
+    document.getElementById(Nimg1).classList.remove("hidden");
+    document.getElementById(Nimg2).classList.remove("hidden");
+    document.getElementById(Nimg3).classList.remove("hidden");
+
   }
 }
 
 document.getElementById('card1').addEventListener('click', () => {
-  handleCardClick('sound1', 'img1', 'hover01');
+  handleCardClick('sound1', 'img1', 'hover01', 'hover02', 'hover03', 'hover04', 'img2', 'img3', 'img4');
 });
 
 document.getElementById('card2').addEventListener('click', () => {
-  handleCardClick('sound2', 'img2', 'hover02');
+  handleCardClick('sound2', 'img2', 'hover02', 'hover01', 'hover03', 'hover04', 'img1', 'img3', 'img4');
 });
 
 document.getElementById('card3').addEventListener('click', () => {
-  handleCardClick('sound3', 'img3', 'hover03');
+  handleCardClick('sound3', 'img3', 'hover03', 'hover01', 'hover02', 'hover04', 'img1', 'img2', 'img4');
 });
 
 document.getElementById('card4').addEventListener('click', () => {
-  handleCardClick('sound4', 'img4', 'hover04');
+  handleCardClick('sound4', 'img4', 'hover04', 'hover01', 'hover02', 'hover03', 'img1', 'img2', 'img3');
 });
 
 updateDisplay();
